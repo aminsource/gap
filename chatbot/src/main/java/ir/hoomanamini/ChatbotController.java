9e5dc1861552360146c149affce948e97e12453e
Houman Amini <h.amini@pep.co.ir>
2024-09-18 06:50:15 +0330

    add chatbot module

--- chatbot/src/main/java/ir/hoomanamini/ChatbotController.java
+++ chatbot/src/main/java/ir/hoomanamini/ChatbotController.java
@@ -1,11 +1,9 @@
 package ir.hoomanamini;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1")
 class ChatbotController {
 
     private final ChatbotService chatbotService;
