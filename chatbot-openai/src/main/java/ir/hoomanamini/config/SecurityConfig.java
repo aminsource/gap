56c703f2612a23e3d670811d65bd4838e84deef3
Houman Amini <h.amini@pep.co.ir>
2025-01-17 15:31:33 +0330

    fix cors error

--- chatbot-openai/src/main/java/ir/hoomanamini/config/SecurityConfig.java
+++ chatbot-openai/src/main/java/ir/hoomanamini/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8095", "http://gapgoft.ir", "https://gapgoft.ir")); // Specify your allowed origins
+        configuration.addAllowedOriginPattern("*://gapgoft.ir"); // Matches both HTTP and HTTPS
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
